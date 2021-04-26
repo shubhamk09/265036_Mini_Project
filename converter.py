78932967f8a8d78aaed25ded2de38f4145ea4ec8
Shubham Kalihari <shubhamkalihari09@gmail.com>
2021-04-26 23:00:15 +0530

    added main function

--- converter.py
+++ converter.py
@@ -8,7 +8,7 @@ def get_data():
 
 
 def get_ratio(frm, to):
-    return float(frm.one_dollar) / float(to.one_dollar)
+    return float(to.one_dollar) / float(frm.one_dollar)
 
 
 def convert(frm, to):
@@ -58,9 +58,49 @@ def count_lines():
     return countlines
 
 
-data = get_data()
-# print(get_currency(data, "inr", "yen"))
-# print(to_celsius(99.0))
-# print(to_fahrenheit(32.0))
-show_temp_file()
-print(count_lines())
+def main():
+    print("--------------------Welcome to THe converter app--------------------")
+    print("--------------------------Press 1 to enter--------------------------")
+    choice_1 = int(input())
+
+    if choice_1 == 1:
+        print("------------------Which converter You want to use?------------------")
+        print("1. For Currency Converter\n2. For Temperature Converter")
+        choice_2 = int(input())
+
+        if choice_2 == 1:
+            print("Chose the the currency which you want to convert\n1.inr 2.yen 3.yuan 4.euro 5.aud 6.nzd 7.dhiram "
+                  "8.riyal ")
+            frm = input()
+            print("Chose the the currency which you want to converted to\n1.inr 2.yen 3.yuan 4.euro 5.aud 6.nzd "
+                  "7.dhiram "
+                  "8.riyal ")
+            to = input()
+            data = get_data()
+            print(get_currency(data, frm, to))
+
+        elif choice_2 == 2:
+            print("Chose the option\n1. Fahrenheit to celsius\n2. Celsius to fahrenheit\n3. See the logs")
+            choice_3=int(input())
+
+            if choice_3 == 1:
+                fahrenheit = float(input("Enter temperature in fahrenheit:\n"))
+                print("Temperature in celsius is: "+str(to_celsius(fahrenheit)))
+
+            elif choice_3 == 2:
+                celsius = float(input("Enter temperature in celsius:\n"))
+                print("Temperature in celsius is: " + str(to_fahrenheit(celsius)))
+
+            elif choice_3 == 3:
+                show_temp_file()
+                print("Total conversion: "+str(count_lines()))
+
+            else:
+                print("Wrong input!")
+
+    else:
+        print("---------------------------Thank you!-------------------------------")
+
+
+if __name__ == "__main__":
+    main()
